7f124f38b187b101cd88311d6ee86d7de39914be
chintan <chintan.sakhiya@improwised.com>
2023-02-14 10:47:11 +0530

    struct

--- main.go
+++ main.go
@@ -22,11 +22,18 @@ func (i *circle) test(j float64) {
 func main() {
 
 	u := circle{3.14, &point{5, 6}}
-	// fmt.Println(*u.pointer, &u.pointer, u.pointer)
+	fmt.Println(*u.pointer, &u.pointer, u.pointer)
 
-	fmt.Println(u.pointer, &u.pointer, *u.pointer)
+	fmt.Println(u.pointer, &u)
 	u.test(4.14)
 
 	fmt.Println(u)
+
+}
+
+func for1(k []int) {
+	for i, item := range k {
+		fmt.Println("this is i", i, "this is item", item)
+	}
 
 }
