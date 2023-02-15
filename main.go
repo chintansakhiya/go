a467335e8efb229f9c3f59b7435c38a4aecb9b45
chintan <chintan.sakhiya@improwised.com>
2023-02-15 11:00:33 +0530

    time formating

--- main.go
+++ main.go
@@ -1,47 +1,12 @@
 package main
 
-import "fmt"
-
-type shape interface {
-	area() float32
-}
-
-type circle struct {
-	r float32
-}
-
-type rect struct {
-	h float32
-	w float32
-}
-
-func (c circle) area() float32 {
-	return 2 * 3.1 * c.r
-}
-
-func (r rect) area() float32 {
-	return r.h * r.w
-}
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 
-	a := circle{10.2}
-	b := rect{10, 5}
-	c := circle{10.2}
-	d := circle{10.2}
-	e := circle{10.2}
-	f := circle{10.2}
-	g := circle{10.2}
-	h := circle{10.2}
-
-	// x := a.area()
-	// y := b.area()
-	// fmt.Println(x, y)
-	shapes := []shape{a, b, c, d, e, f, g, h}
-
-	for _, item := range shapes {
-
-		fmt.Println(item.area())
-	}
-
+	t := time.Now()
+	fmt.Println(t.Format("Monday, Jan 2 2006"))
 }
