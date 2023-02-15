07a85d40a78ac3a6349e1ca185b68f4827ba6c7a
chintan <chintan.sakhiya@improwised.com>
2023-02-15 10:43:23 +0530

    map

--- main.go
+++ main.go
@@ -3,36 +3,56 @@ package main
 import (
 	//"errors"
 	"fmt"
-	"strconv"
 )
 
 func main() {
 
-	a, perror := devide(6, 2)
-	if perror == nil {
-		fmt.Println("division is", a)
-	} else {
-		fmt.Println(a, perror)
-	}
+	x := map[string]int{}
+	z := 10
+	y := &z
+	*y = 12
+	x["key"] = 10
+	x["value"] = 10
+	x["key"] = 13
+
+	fmt.Println(x)
+
+	// fmt.Println("start")
+	// defer func() {
+
+	// 	if r := recover(); r != nil {
+	// 		fmt.Println("panic ")
+	// 	}
+
+	// }()
+
+	// fmt.Println("end")
+	//panic("this is panic")
+	// a, perror := devide(6, 2)
+	// if perror == nil {
+	// 	fmt.Println("division is", a)
+	// } else {
+	// 	fmt.Println(a, perror)
+	// }
 
 }
 
-type CustomError struct {
-	mess  string
-	value int
-}
+// type CustomError struct {
+// 	mess  string
+// 	value int
+// }
 
-func (c CustomError) Error() string {
-	fmt.Println("from error")
-	return c.mess + " " + strconv.Itoa(c.value)
+// func (c CustomError) Error() string {
+// 	fmt.Println("from error")
+// 	return c.mess + " " + strconv.Itoa(c.value)
 
-}
+// }
 
-func devide(x, y float64) (float64, error) {
+// func devide(x, y float64) (float64, error) {
 
-	if y == 0 {
-		return x, CustomError{"this is error", -1}
+// 	if y == 0 {
+// 		return x, CustomError{"this is error", -1}
 
-	}
-	return float64(x) / float64(y), nil
-}
+// 	}
+// 	return float64(x) / float64(y), nil
+// }
